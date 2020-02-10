f9aac03b6d4bbd1987b70518857032a7f8ef0c69
GodsEye-07 <vermaayush.verma76@gmail.com>
2020-02-10 16:00:15 +0530

    first commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Ayush verma on 10/02/20.
+//  Copyright © 2020 Ayush verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
